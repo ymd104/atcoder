f23d6a1985efa4d8b02635a70bed11b6d7caf2d0
山田瑛平 <yamada@yamadayoheiMacBook-Air.local>
2019-08-28 21:40:11 +0900

    add new file

---
+++ abc138a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(long i=0; i<n; i++)
+
+
+void solve(){
+    vector<int> a;
+    cout << pow(2,3) << endl;
+}
+
+int main(){
+  solve();
+  return 0;
+}
